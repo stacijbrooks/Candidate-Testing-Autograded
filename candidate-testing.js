2f32c2a50fe8e82239f3eeccc4d8dc9d1b700122
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-21 21:30:17 -0600

    Candidate Testing Part Two

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,33 +11,48 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
   // TODO 1.1b: Ask for candidate's name //
 function askForName() {
-  candidateName = prompt("What is your name?")
-
-}
+  candidateName = input.question("What is your name?");
+  console.log(candidateName);
+} 
 
 function askQuestion() {
-question = prompt("Who was the first American woman in space? ");
-}
+//question = prompt("Who was the first American woman in space? ");
+
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+for (let i = 0; i < questions.length; i++)  {
+  let userAnswer = input.question(questions[i]);
+  candidateAnswers.push(userAnswer);
+}
+}
 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
+/*if (candidateAnswer === correctAnswer) {
   console.log("Correct!");
  } else {
   console.log("Sorry,this is incorrect! Please try again.")
-}
-
+}*/
+console.log(`User response ${candidateAnswers}, correct response is ${correctAnswers}`)
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -48,7 +63,7 @@ if (candidateAnswer === correctAnswer) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello," (candidateName));
+   console.log("Hello," + (candidateName));
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
